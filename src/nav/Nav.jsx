5cd2b52da40cab7e070edef9023e4934f5b1cbd4
Ashish Gupta <gkashish1985@gmail.com>
2024-12-01 14:59:40 +0530

    health:feat nav

---
+++ src/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className='w-[95%] h-[72px] bg-[#ffff] mx-auto rounded-md' >
+      
+    </nav>
+  )
+}
+
+export default Nav
