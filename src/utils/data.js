a3a57a5e3aeccd661c0d08cef78970bbb74b5d3f
Ashish Gupta <gkashish1985@gmail.com>
2024-12-01 16:10:04 +0530

    Feat:nav

---
+++ src/utils/data.js
@@ -0,0 +1,39 @@
+export const navItems = [
+    {
+        id: 1,
+        name: 'Overview',
+        url: '/',
+        icon: "/assets/homeIcon.svg",
+        alt : 'overView'
+    },
+    {
+        id: 2,
+        name: 'Patients',
+        url: '/patients',
+        icon: "/assets/groupic.svg",
+        alt : 'patients'
+    },
+    {
+        id: 3,
+        name: 'Shedule',
+        url: '/shedule',
+        icon: "/assets/calendar.svg",
+        alt : 'shedule'
+    },
+    {
+        id: 4,
+        name: 'Message',
+        url: '/inbox',
+        icon: "/assets/message.svg",
+        alt : 'message'
+    },
+    {
+        id: 5,
+        name: 'Transactions',
+        url: '/transactions',
+        icon: "/assets/card.svg",
+        alt : 'profile'
+    },
+    
+
+]
