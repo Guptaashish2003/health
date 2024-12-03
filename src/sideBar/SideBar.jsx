cbf740e1f487b7181f168b59f222a0d1e711d950
rajnish <rajneeshkumar9674737@gmail.com>
2024-12-03 14:53:19 +0530

    sidebar responsive

--- src/sideBar/SideBar.jsx
+++ src/sideBar/SideBar.jsx
@@ -1,26 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./_component/Card";
 
-const SideBar = ({data,id,setId,setPatientData}) => {
-    useEffect(()=>{
-        setPatientData(data[id]);
-      }
-      ,[id])
+const SideBar = ({ data, id, setId, setPatientData }) => {
+  useEffect(() => {
+    setPatientData(data[id]);
+  }, [id]);
+  const [open, setOpen] = useState(true);
   return (
-    <div className=' w-[20%]  max-w-[367px]  bg-white rounded-3xl max-h-[90vh] overflow-scroll'>
-        <div className='flex p-4 justify-between font-semibold border '>
-            <h3 className=' text-lg'>Patients</h3>
-            <div className='w-4'>
-                <img src="/assets/search_FILL0_wght300_GRAD0_opsz24.svg" alt="search" width={50} height={50} className='w-full h-full ' />
-            </div>
+    <div className="max-xl:w-16 w-[20%]  max-w-[367px]  bg-white rounded-3xl max-h-[90vh] overflow-scroll">
+      <div className="flex max-xl:p-3 max-xl:justify-center p-4 justify-between font-semibold border ">
+        <h3 className="max-xl:hidden text-lg">Patients</h3>
+        <div className="w-4">
+          <img
+            src="/assets/search_FILL0_wght300_GRAD0_opsz24.svg"
+            alt="search"
+            width={50}
+            height={50}
+            className="w-full h-full "
+          />
         </div>
-        <div>
-            {data?.map((item,index)=>{
-                return <Card setId={setId} key={index} index={index} profile={item.profile_picture} name={item.name} gender={item.gender} age={item.age} />
-            })}
-            
-        </div>
-      
+      </div>
+      <div>
+        {data?.map((item, index) => {
+          return (
+            <Card
+              setId={setId}
+              key={index}
+              index={index}
+              profile={item.profile_picture}
+              name={item.name}
+              gender={item.gender}
+              age={item.age}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/sideBar/_component/Card.jsx
+++ src/sideBar/_component/Card.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Card = ({profile,name,gender,age,index,setId}) => {
 
   return (
-    <div onClick={()=>setId(index)} className="flex justify-between items-center px-6 py-2 cursor-pointer" tabIndex={0}>
-      <div className="flex gap-x-2 items-center justify-center capitalize">
+    <div onClick={()=>setId(index)} className="flex justify-between items-center max-xl:justify-center xl:px-6 py-2 cursor-pointer" tabIndex={0}>
+      <div className="flex xl:gap-x-2 items-center justify-center capitalize">
         <img
           src={profile}
           alt="profile"
-          className="size-10 rounded-full object-cover "
+          className="size-10 rounded-full object-cover border-6 "
         />
-        <div className="text-sm font-semibold">
+        <div className="text-sm font-semibold max-xl:hidden">
           <p>{name}</p>
           <p className="text-xs text-[#707070] ">{gender}: {age}</p>
         </div>
       </div>
-      <div className="w-4 cursor-pointer">
+      <div className="w-4 max-xl:hidden">
         <img
           src="/assets/more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
           alt="more"
